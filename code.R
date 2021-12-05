05f0b568242631dcb1b719f852d6609520a0da3d
(nithinmkp) <write2nithinm@gmail.com>
2021-12-05 20:26:08 +0530

    added a functions script for souring custom functions- cleaner script

--- code.R
+++ code.R
@@ -7,41 +7,13 @@ sapply(packages,library,character.only=T)
 plot_dir<-"ggplots"
 if (!dir.exists(plot_dir)) {dir.create(plot_dir)}
 
+#Use functions
+source("functions.R")
+
 #read data
 sheets<-import_list("KDF.xlsx")
 names(sheets)<-c("GDP_PC","GDP_PC_gr","RI_PC")
 list2env(sheets,envir = globalenv())
-
-#KD plot function
-kd_fn<- function(df,xvar,catvar,alph=0.2,siz=1.5,xlab,ylab="Density",
-                 title=NA,leg_title,gr=F,legend.pos.h=0.29,
-                 legend.pos.t=1,ylab_acur=2,leg_row=3){
-  ncolors<-df %>% select({{catvar}}) %>% n_distinct()
-  my_colors<-colorRampPalette(brewer.pal(8,"Set2"))(ncolors)
-  p1<-df %>% ggplot(aes(x=.data[[xvar]]))+
-    geom_density(aes(color=.data[[catvar]]),
-                 alpha=alph,
-                 size=siz,
-                 key_glyph="smooth")+
-    theme_bw()+
-    theme(legend.direction  ="vertical",
-          legend.background = element_rect(size=0.5, linetype="solid", 
-                                           colour ="darkblue"),
-          legend.text = element_text(size = 9),
-          legend.title.align = 0.5,
-          legend.justification = "top",
-          legend.position = c(legend.pos.h,legend.pos.t))+
-    guides(col=guide_legend(nrow=leg_row, byrow=T))+
-    labs(x=xlab,y=ylab,
-         color=leg_title)+
-    scale_color_manual(values = my_colors)
-  if(gr==T){
-    p1+scale_x_continuous(labels = label_percent(accuracy = ylab_acur))
-  }else{
-    p1
-  }
-}
-
 
 #some wrangling on sheet3- Not in standard format
 names(RI_PC)<-RI_PC[2,]
@@ -57,14 +29,14 @@ GDP_PC_gr<-GDP_PC_gr %>%
 
 
 
-#converting columns
-convert_fn<-function(df, col_ind,fn) {
-  df <- df %>% mutate(across(.cols = col_ind, .fns = fn))
-}
-
+#list of dataframes
 df<-list(GDP_PC,GDP_PC,RI_PC,RI_PC,GDP_PC_gr,GDP_PC_gr)
+
+#converting columns
 df<-map(df,convert_fn,col_ind=c(1,2),fn=as.factor)
 df<-map(df,convert_fn,col_ind=3,fn=as.numeric)
+
+#plots
 k<-tibble(catvar=rep(c("Year","Country"),times=3),
        leg_title=rep(c("Period","Country"),times=3),
        legend.pos.h=c(0.29,0.33,0.29,0.73,0.29,0.274),
@@ -79,7 +51,7 @@ k<-tibble(catvar=rep(c("Year","Country"),times=3),
   
   
   
-  k %>% 
+k %>% 
   mutate(filename = paste0("ggplots/", xlab, leg_title,".png")) %>% 
     select(plot,filename) %>% 
   pwalk(ggsave,width = 22.5,
---
+++ functions.R
@@ -0,0 +1,37 @@
+
+# Functions ---------------------------------------------------------------
+
+#KD plot function
+kd_fn<- function(df,xvar,catvar,alph=0.2,siz=1.5,xlab,ylab="Density",
+                 title=NA,leg_title,gr=F,legend.pos.h=0.29,
+                 legend.pos.t=1,ylab_acur=2,leg_row=3){
+  ncolors<-df %>% select({{catvar}}) %>% n_distinct()
+  my_colors<-colorRampPalette(brewer.pal(8,"Set2"))(ncolors)
+  p1<-df %>% ggplot(aes(x=.data[[xvar]]))+
+    geom_density(aes(color=.data[[catvar]]),
+                 alpha=alph,
+                 size=siz,
+                 key_glyph="smooth")+
+    theme_bw()+
+    theme(legend.direction  ="vertical",
+          legend.background = element_rect(size=0.5, linetype="solid", 
+                                           colour ="darkblue"),
+          legend.text = element_text(size = 9),
+          legend.title.align = 0.5,
+          legend.justification = "top",
+          legend.position = c(legend.pos.h,legend.pos.t))+
+    guides(col=guide_legend(nrow=leg_row, byrow=T))+
+    labs(x=xlab,y=ylab,
+         color=leg_title)+
+    scale_color_manual(values = my_colors)
+  if(gr==T){
+    p1+scale_x_continuous(labels = label_percent(accuracy = ylab_acur))
+  }else{
+    p1
+  }
+}
+
+#type conversion function
+convert_fn<-function(df, col_ind,fn) {
+  df <- df %>% mutate(across(.cols = col_ind, .fns = fn))
+}
