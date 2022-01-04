86709827bbf895543a4e92e25e37847fdec1cd35
(nithinmkp) <write2nithinm@gmail.com>
2022-01-04 18:29:01 +0530

    changed code for package loading

--- code.R
+++ code.R
@@ -3,6 +3,7 @@ package_fn<-function(pkg){
   new.pkg<-pkg[!(pkg %in% installed.packages()[,"Package"])]
   if(length(new.pkg)){
     install.packages(new.pkg,dependencies = T)
+  }else{
     sapply(pkg,library,character.only=T)
   }
 }
