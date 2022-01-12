162793c446844a9f8f139305b1e41279610bcc13
(nithinmkp) <write2nithinm@gmail.com>
2022-01-12 08:41:54 +0530

    cleaned up code to use cutom package function to load packages

--- code.R
+++ code.R
@@ -1,12 +1,5 @@
 #Packages
-package_fn<-function(pkg){
-  new.pkg<-pkg[!(pkg %in% installed.packages()[,"Package"])]
-  if(length(new.pkg)){
-    install.packages(new.pkg,dependencies = T)
-  }else{
-    sapply(pkg,library,character.only=T)
-  }
-}
+source("functions.R")
 packages<-c("tidyverse","readxl","MetBrewer","scales","rio","RColorBrewer")
 package_fn(packages)
 
